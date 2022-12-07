eb591bc9ebf2c9606be70de53f8df4aa9bafc4ef
Saul Ruiz <saulruiz730@gmail.com>
2022-12-07 16:50:59 -0600

    created serializer, models and views api for products

--- backend/products/apps.py
+++ backend/products/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CapstoneConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'capstone_project'
+    name = 'products'
---
+++ backend/products/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from authentication.models import User
+
+# Create your models here.
+
+
+class Product(models.Model):
+    title = models.CharField(max_length = 225)
+    description = models.CharField(max_length = 225)
+    price = models.DecimalField(max_digits = 7, decimal_places = 2)
+    inventory_quantity = models.IntegerField()
